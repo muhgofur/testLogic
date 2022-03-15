bf1d13756ea707fbd6d4c3165a114d814a3bfab2
gofurmzq <muhamadgofur88@gmail.com>
2022-03-15 11:20:58 +0700

    logic test

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def logic():
-    a = 1+1
-    print(a)
-
+def iterasi():
+    N = 50
+    result = ""
+    for i in range(int(N)):
+        if i <= 19:
+            if i <= 1:
+                result += "1, 2, "
+            elif i ==2:
+                result += "frontend, "
+            elif i == 3:
+                result += "3, "
+            elif i > 3 and i < 6:
+                result += "frontend, backend, "
+            elif i == 6:
+                result +=  "frontend, "
+            elif i > 6 and i < 9:
+                result += "frontend, backend, "
+            elif i == 9 and i < 11:
+                result += "f"                
+    
+    print(result)
+        
 if __name__ == "__main__":
-    logic()
+    iterasi()
